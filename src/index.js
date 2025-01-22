f0a9c9453db64fca6695e30dca0ece98e6402dee
ValerieWaceera <annvalerie54@gmail.com>
2025-01-22 18:15:02 +0300

    Update a few details on HTML and also added an Icon

--- src/index.js
+++ src/index.js
@@ -6,33 +6,37 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date= new Date(response.data.time * 1000);
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = formate(date);
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
   temperatureElement.innerHTML = Math.round(temperature);
-
 }
-function formatDate(date){
-    let minutes= date.getMinutes();
-    let hours = date.getHours();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wenesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wenesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-]
-let day =days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
 
-if (minutes<)
-return `${day} ${hours}: ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "f61et4eca5db12383440073537b04oaf";
